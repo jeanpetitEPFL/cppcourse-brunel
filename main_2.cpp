d986e1ccc2d528032c80f6b31fe0efc99dc58fd1
jeanpetitEPFL <32450758+jeanpetitEPFL@users.noreply.github.com>
2017-10-14 18:49:24 +0200

    Add files via upload

--- main_2.cpp
+++ main_2.cpp
@@ -4,76 +4,11 @@
 #include <fstream>
 #include <iostream>
 using namespace std;
-/*
-void ifneverspiked(Neuron& a, Neuron& b)
-{
-	if (a.getSpikesTime().empty())
-		{
-			b.setMembPot(b.getMembpot());
-		}	
-}
 
-void ifalreadyspiked(Neuron& a, Neuron& b)
-{
-if(!a.getSpikesTime().empty())		
-		{
-			double New;
-			size_t x;
-			x= a.getSpikesTime().size()-1;
-			if(a.getSpikesTime()[x]== b.getInternalTime())
-			{ 
-				b.setbuffer();
-				//New+=0.4; 
-				
-			}
-			//New=b.getbuffer()[3];	
-			//b.setMembPot(b.getMembpot()+New);	
-			b.updatebuffer();
-		}
-	
-}
-
-void recieve(Neuron& b, Neuron& a)
-{
-		ifneverspiked(a,b);
-		ifalreadyspiked(a,b);
-		
-}
-
-
-void simulation (Neuron& a, Neuron& b)
-	{	
-		double I (1.5);
-		ofstream myfile;
-		ofstream myfile2;
-		myfile.open ("neurontest.txt");		
-		myfile2.open ("neurontest2.txt");
-
-		double simtime(0.0);
-		double tstop (200);
-		
-		while (simtime < tstop)
-		{
-			a.update(I);
-			b.update(0);
-			recieve(b,a);
-			recieve(a,b);
-			myfile << a.getMembpot()<<endl;
-			myfile2 << b.getMembpot()<<endl;
-
-			simtime+=a.getH();
-		}
-	cout<< a.getNbSpikes()<<endl;
-	cout<< b.getNbSpikes()<<endl;
-
-	myfile.close();
-	myfile2.close();
-	}	
-*/
 int main()
 {
-	Neuron a(2,0.4);
-	Neuron b(3,0.4);
+	Neuron a(2,4);
+	Neuron b(3,4);
 	Network alpha(a,b);
 	
 	alpha.simulation(0,200, 1.5);
