6c817608b35ad73796bf6df4fbdb7725e4678b13
Jeanpetit Florent <32450758+jeanpetitEPFL@users.noreply.github.com>
2017-11-06 18:29:26 +0100

    corrected version of test
    #include correct for gtest

--- neuron_test.cpp
+++ neuron_test.cpp
@@ -1,8 +1,9 @@
 #include <iostream>
-#include "neuron_2.hpp"
-#include "network_2.hpp"
-#include "googletest-master/googletest/include/gtest/gtest.h"
-
+#include "neuron.hpp"
+#include "network.hpp"
+//#include "/usr/include/gtest.h"
+#include <gtest/gtest.h>
+//#include "googletest-master/googletest/include/gtest/gtest.h"
 
 /**
  *	test one neuron
@@ -76,11 +77,12 @@ TEST (SingleNeuronTest, Neuron_SpikeTime04)
 	
 	for (int i = 0; i < 5000; i++)
 	{
-		neuronA.update(1.00,0);
+		neuronA.update(1.0,0);
 	}
 	
 	EXPECT_EQ(0, neuronA.getNbSpikes());
 }
+
 /**
  *	test two neurons interaction
  *	buffer of neuronB if A and B connected
@@ -103,24 +105,48 @@ TEST(TwoNeuronTest, Buffer)
 		}
 		neuronB.update(0,0);
 	}
-	
 	EXPECT_NEAR(0.1, neuronB.getMembpot(), 10e-3); 
-
 }
 
 /**
  *	test network
- *	size of network
+ *	test size of network
  */ 
-TEST(Networktest, Network_Time )
+TEST(NetworkTest, Network_Size )
 {
-
 	Network net(10000,2500);
-	
-EXPECT_EQ(12500, net.getNetworkSize());
-net.simulation(0,100,0,5,2);
-EXPECT_EQ(1000, net.getGlobalTime());
+	EXPECT_EQ(12500, net.getNetworkSize());
+}
 
+/**
+ *	test network
+ *	test nature of neuron inside network
+ */ 
+TEST(NetworkTest, Nature_Of_Neurons)
+{
+	Network net(10000,2500);
+	int True(0);
+	int False(0);
+	for (auto& element : net.getNetwork())
+	{
+		if(element->getEx())
+		{  ++ True; }
+		if(!element->getEx())
+		{  ++False; }
+	}
+	EXPECT_EQ(10000, True);
+	EXPECT_EQ(2500, False);
+}
+
+/**
+ *	test network
+ *	test global time of network
+ */ 
+TEST(NetworkTest, Network_Time)
+{
+	Network net(10000,2500);
+	net.simulation(0,100,0,5,2);
+	EXPECT_EQ(1000, net.getGlobalTime());
 
 }
 
